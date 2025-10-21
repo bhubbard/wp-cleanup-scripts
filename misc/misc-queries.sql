d292f0ab20e90fc47bc03fe673cb35820a92ce4b
Brandon Hubbard <bhubbard@users.noreply.github.com>
2025-10-21 08:32:42 -0700

    Empty disallowed keys list in wp_options
    This clears the list of disallowed comment keys to prevent bloat.

--- misc/misc-queries.sql
+++ misc/misc-queries.sql
@@ -17,3 +17,7 @@ DELETE FROM `wp_options` WHERE `option_value` IS NULL OR `option_value` = '';
 -- 5. Delete all transients
 -- This removes all transient options from the options table, which can help clear cached data.
 DELETE FROM `wp_options` WHERE `option_name` LIKE '_transient_%' OR `option_name` LIKE '_site_transient_%';
+
+-- 6. Empty the disallowed keys list
+-- This clears the list of disallowed comment keys, which can sometimes become bloated.
+UPDATE `wp_options` SET `option_value` = '' WHERE `option_name` = 'disallowed_keys';
